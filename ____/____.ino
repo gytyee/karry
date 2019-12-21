3c78d785c15dbad1d7bacea6730617f8879a3348
gytyee <1243417004@qq.com>
2019-12-21 22:36:24 +0800

    2

---
+++ ____/____.ino
@@ -0,0 +1,212 @@
+/*
+原版作者: 张林羽觐
+
+由于新版arduino IDE for mCookie中对SHT2x.h和ESP8266.h进行了修改
+因此对原有代码进行修改 不需要任何外部头文件
+*/
+
+/**
+ * 需要做替换的部分!!!!!!!!!
+ * 
+*/
+#define SSID        "vivo S1 Pro" //改为你的热点名称, 不要有中文
+#define PASSWORD    "2468776998"//改为你的WiFi密码Wi-Fi密码
+#define DEVICEID    "577134872" //OneNet上的设备ID
+String apiKey = "XWZTpNeRkbdFJl9CqOEyQBicEuM= ";//与你的设备绑定的APIKey
+#include <U8glib.h>
+const unsigned char PROGMEM dao[] = {
+0x00,0x04,0xFF,0x84,0x08,0x04,0x10,0x24,0x22,0x24,0x41,0x24,0xFF,0xA4,0x08,0xA4,
+0x08,0x24,0x08,0x24,0x7F,0x24,0x08,0x24,0x08,0x04,0x0F,0x84,0xF8,0x14,0x40,0x08
+};
+const unsigned char PROGMEM dian[]={
+0x02,0x00,0x02,0x00,0x02,0x00,0x03,0xFC,0x02,0x00,0x02,0x00,0x3F,0xF0,0x20,0x10,
+0x20,0x10,0x20,0x10,0x3F,0xF0,0x00,0x00,0x24,0x88,0x22,0x44,0x42,0x44,0x80,0x04
+};
+const unsigned char PROGMEM la[]={
+0x01,0x20,0x01,0x10,0x71,0x10,0x51,0x7E,0x57,0x80,0x51,0x04,0x51,0x44,0x51,0xC4,
+0x51,0x24,0x53,0x24,0x75,0x28,0x51,0x28,0x01,0x08,0x01,0x10,0x05,0x7E,0x02,0x00
+};
+const unsigned char PROGMEM shui[]={
+0x00,0x10,0x00,0x78,0xF3,0xC0,0x90,0x40,0x90,0x40,0xF7,0xFC,0x92,0x48,0x92,0x48,
+0xF7,0xFE,0x92,0x48,0x92,0x48,0x97,0xFC,0xF0,0x40,0x90,0x40,0x07,0xFC,0x00,0x00
+};
+const unsigned char PROGMEM jiao[]={
+0x22,0x08,0x11,0x10,0x00,0x20,0x7F,0xFE,0x40,0x02,0x80,0x04,0x1F,0xF0,0x10,0x10,
+0x11,0x10,0x11,0x10,0x11,0x10,0x12,0x90,0x02,0x80,0x04,0x84,0x18,0x84,0x60,0x7C
+};
+#define INTERVAL_LCD             20             //定义OLED刷新时间间隔  
+unsigned long lcd_time = millis();                 //OLED刷新时间计时器
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);     //设置OLED型号  
+//-------字体设置，大、中、小
+#define setFont_L u8g.setFont(u8g_font_7x13)
+#define setFont_M u8g.setFont(u8g_font_fixed_v0r)
+#define setFont_S u8g.setFont(u8g_font_fixed_v0r)
+#define setFont_SS u8g.setFont(u8g_font_fub25n)
+
+/***/
+#define HOST_NAME   "api.heclouds.com"
+#define HOST_PORT   (80)
+#define INTERVAL_SENSOR   5000             //定义传感器采样时间间隔  597000
+#define INTERVAL_NET      5000             //定义发送时间
+//传感器部分================================   
+#include <Wire.h>                                  //调用库  
+#include <ESP8266.h>
+#include <I2Cdev.h>                                //调用库  
+/*******光照*******/
+#define  sensorPin_1  A0
+#define IDLE_TIMEOUT_MS  1000     // Amount of time to wait (in milliseconds) with no data 
+                                   // received before closing the connection.  If you know the server
+                                   // you're accessing is quick to respond, you can reduce this value.
+
+//WEBSITE     
+char buf[10];
+
+#define INTERVAL_sensor 2000
+unsigned long sensorlastTime = millis();
+
+float  lightnessOLED;
+
+#define INTERVAL_OLED 1000
+
+String mCottenData;
+String jsonToSend;
+
+//3,传感器值的设置 
+float sensor_lux;                    //传感器温度、湿度、光照   
+char  sensor_lux_c[7] ;    //换成char数组传输
+#include <SoftwareSerial.h>
+#define EspSerial mySerial
+#define UARTSPEED  9600
+SoftwareSerial mySerial(2, 3); /* RX:D3, TX:D2 */
+ESP8266 wifi(&EspSerial);
+//ESP8266 wifi(Serial1);                                      //定义一个ESP8266（wifi）的对象
+unsigned long net_time1 = millis();                          //数据上传服务器时间
+unsigned long sensor_time = millis();                        //传感器采样时间计时器
+
+//int SensorData;                                   //用于存储传感器数据
+String postString;                                //用于存储发送数据的字符串
+//String jsonToSend;                                //用于存储发送的json格式参数
+
+
+void setup(void)     //初始化函数  
+{       
+  //初始化串口波特率  
+    Wire.begin();
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    Serial.print(F("setup begin\r\n"));
+    delay(100);
+    pinMode(sensorPin_1, INPUT);
+
+  WifiInit(EspSerial, UARTSPEED);
+
+  Serial.print(F("FW Version:"));
+  Serial.println(wifi.getVersion().c_str());
+
+  if (wifi.setOprToStationSoftAP()) {
+    Serial.print(F("to station + softap ok\r\n"));
+  } else {
+    Serial.print(F("to station + softap err\r\n"));
+  }
+
+  if (wifi.joinAP(SSID, PASSWORD)) {
+    Serial.print(F("Join AP success\r\n"));
+
+    Serial.print(F("IP:"));
+    Serial.println( wifi.getLocalIP().c_str());
+  } else {
+    Serial.print(F("Join AP failure\r\n"));
+  }
+
+  if (wifi.disableMUX()) {
+    Serial.print(F("single ok\r\n"));
+  } else {
+    Serial.print(F("single err\r\n"));
+  }
+
+  Serial.print(F("setup end\r\n"));
+    
+  
+}
+void loop(void)     //循环函数  
+{   
+  if (sensor_time > millis())  sensor_time = millis();  
+    
+  if(millis() - sensor_time > INTERVAL_SENSOR)              //传感器采样时间间隔  
+  {  
+    getSensorData();                                        //读串口中的传感器数据
+    sensor_time = millis();
+  }  
+
+    
+  if (net_time1 > millis())  net_time1 = millis();
+  
+  if (millis() - net_time1 > INTERVAL_NET)                  //发送数据时间间隔
+  {                
+    updateSensorData();                                     //将数据上传到服务器的函数
+    net_time1 = millis();
+  }
+  
+}
+
+void getSensorData(){  
+    sensor_lux = analogRead(A0);    
+    delay(1000);
+    dtostrf(sensor_lux, 3, 1, sensor_lux_c);
+}
+void updateSensorData() {
+  if (wifi.createTCP(HOST_NAME, HOST_PORT)) { //建立TCP连接，如果失败，不能发送该数据
+    Serial.print("create tcp ok\r\n");
+
+ jsonToSend="{\"Light\":";
+    dtostrf(sensor_lux,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+="}";
+
+
+
+    postString="POST /devices/";
+    postString+=DEVICEID;
+    postString+="/datapoints?type=3 HTTP/1.1";
+    postString+="\r\n";
+    postString+="api-key:";
+    postString+=apiKey;
+    postString+="\r\n";
+    postString+="Host:api.heclouds.com\r\n";
+    postString+="Connection:close\r\n";
+    postString+="Content-Length:";
+    postString+=jsonToSend.length();
+    postString+="\r\n";
+    postString+="\r\n";
+    postString+=jsonToSend;
+    postString+="\r\n";
+    postString+="\r\n";
+    postString+="\r\n";
+
+  const char *postArray = postString.c_str();                 //将str转化为char数组
+  Serial.println(postArray);
+  wifi.send((const uint8_t*)postArray, strlen(postArray));    //send发送命令，参数必须是这两种格式，尤其是(const uint8_t*)
+  Serial.println("send success");   
+     if (wifi.releaseTCP()) {                                 //释放TCP连接
+        Serial.print("release tcp ok\r\n");
+        } 
+     else {
+        Serial.print("release tcp err\r\n");
+        }
+      postArray = NULL;                                       //清空数组，等待下次传输数据
+  
+  } else {
+    Serial.print("create tcp err\r\n");
+  }
+  // put your main code here, to run repeatedly:
+  u8g.firstPage();
+  do {
+    setFont_S;
+    u8g.drawBitmapP(0, 0, 2, 16, dao);
+    u8g.drawBitmapP(20,0,2,16,dian);
+    u8g.drawBitmapP(40,0,2,16,la);
+    u8g.drawBitmapP(20,30,2,16,shui);
+    u8g.drawBitmapP(40,30,2,16,jiao);
+    u8g.drawBitmapP(60,30,2,16,la);
+  }while (u8g.nextPage());
+}
